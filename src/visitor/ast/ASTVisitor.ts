33fd8ef516b5779284905aeb333f1ab72334f300
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2023-10-18 03:19:00 -0700

    ASTVisitor setup

--- src/visitor/ast/ASTVisitor.ts
+++ src/visitor/ast/ASTVisitor.ts
@@ -1,6 +1,6 @@
 import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor';
-import { SubjektVisitor } from "../antlr";
-import { ASTModel } from "../types";
+import { SubjektVisitor } from "../../antlr";
+import { ASTModel } from "../../types";
 
 export class ASTVisitor
     extends AbstractParseTreeVisitor<ASTModel>
--- src/visitor/index.ts
+++ src/visitor/index.ts
@@ -1,2 +1,2 @@
-export * from './ASTVisitor';
+export * from './ast';
 export * from './model';
